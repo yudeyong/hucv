bfb25890e3a708c01a1c15305c4ef7da20f82685
dyyu <yudeyong@yeah.net>
2021-02-02 16:17:29 +0800

    [打包]setup

---
+++ prj_lia/src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="hureco",
+    version='0.6.2',
+    packages=['hureco', 'hureco.agi'],
+    url='',
+    license='',
+    author='Dean',
+    author_email='yudeyong@yeah.net',
+    description='human recognization lib. AGI only for current version'
+)
