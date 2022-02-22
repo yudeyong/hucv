baf5a9627b4c3706d53ac8c17d8138c682e21d70
yudeyong <yudeyong@dkyao.com>
2022-02-22 23:28:15 +0800

    [hureco]换目录名

---
+++ prj_pns/src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="hureco_pns",
+    version='0.7.0',
+    packages=['hureco_pns', 'hureco_pns.agi'],
+    url='',
+    license='',
+    author='Dean',
+    author_email='yudeyong@yeah.net',
+    description='human recognization for pns lib. AGI only for current version'
+)
